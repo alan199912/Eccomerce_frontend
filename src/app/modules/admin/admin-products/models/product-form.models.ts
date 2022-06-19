d4e72daffa74e51ed88a3e0c349f7919c0b9848d
Alan199912 <alan199912@gmail.com>
2022-06-19 17:37:23 -0300

    fix - form by default

--- src/app/modules/admin/admin-products/models/product-form.models.ts
+++ src/app/modules/admin/admin-products/models/product-form.models.ts
@@ -10,16 +10,14 @@ export abstract class ProductForm {
 
   public createEmptyForm(): FormGroup {
     return this.fb.group({
-      name: ['Product 1', [Validators.required, noSpacesValidator]],
-      description: ['description 1', [Validators.required, noSpacesValidator]],
-      richDescription: ['rich desc', [Validators.required, noSpacesValidator]],
-      brand: ['brand', [Validators.required, noSpacesValidator]],
-      // image: ['', [Validators.required, noSpacesValidator]],
-      // images: ['', [Validators.required, noSpacesValidator]],
-      rating: ['2', [Validators.required, noSpacesValidator]],
-      price: [15, [Validators.required, noSpacesValidator]],
-      featured: ['1', [Validators.required, noSpacesValidator]],
-      category: ['2', [Validators.required, noSpacesValidator]],
+      name: ['', [Validators.required, noSpacesValidator]],
+      description: ['', [Validators.required, noSpacesValidator]],
+      richDescription: ['', [Validators.required, noSpacesValidator]],
+      brand: ['', [Validators.required, noSpacesValidator]],
+      rating: ['', [Validators.required, noSpacesValidator]],
+      price: [null, [Validators.required, noSpacesValidator]],
+      featured: ['', [Validators.required, noSpacesValidator]],
+      category: ['', [Validators.required, noSpacesValidator]],
     });
   }
 }
